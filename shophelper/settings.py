2ab3c9cc2815f885de4af5209b4e5557fa77cc1c
koynovli <klaslev14@gmail.com>
2026-05-05 16:26:11 +0300

    Добавлено приложение corsheaders для поддержки CORS. Обновлены настройки Django: включен CorsMiddleware в MIDDLEWARE и добавлены разрешенные источники для локальной разработки. Разрешена передача куки и авторизации.

--- shophelper/settings.py
+++ shophelper/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,3 +138,10 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+# Разрешаем передачу куки и авторизации
+CORS_ALLOW_CREDENTIALS = True
